96c848e0cc9b32fb5691fd88f782fdf9216e1f97
Roney Felipe de Oliveira Miranda <felipeoliveira0743@gmail.com>
2023-03-21 14:42:34 -0400

    Lembrando como manipula o argv

--- wcat.c
+++ wcat.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+int main(int argc, char *argv[]) {
+
+    char arquivoAtual[100];
+
+    int marcador = 5;
+
+    for(int palavra = 0; palavra < argc; palavra++) {
+        
+        printf("Arquivo lido %s", arquivoAtual);
+
+    }
+
+}
